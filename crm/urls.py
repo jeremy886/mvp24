a94d8657b8cce8890f668c9f426bdf5368314ad5
jeremy886 <jeremy886@gmail.com>
2024-08-04 17:19:19 +1000

    MVP - Record Detail View

--- crm/urls.py
+++ crm/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.home, name='home'),
-    path('records', views.record_list, name='record_view'),
+    path('records/', views.record_list, name='record_list'),
+    path('records/<int:pk>/', views.record_detail, name='record_detail'),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Record
+
 
 def home(request):
     records = {"developer": "Jeremy Chen"}
@@ -9,3 +10,9 @@ def home(request):
 def record_list(request):
     records = Record.objects.all()
     return render(request, 'crm/record_list.html', {"records": records})
+
+
+def record_detail(request, pk):
+    record = get_object_or_404(Record, pk=pk)
+    return render(request, 'crm/record_detail.html', {'record': record})
+
