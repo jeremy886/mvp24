4c85f2431f39a0274c52981ebbe5dd082fdc0c7e
jeremy886 <jeremy886@gmail.com>
2024-08-04 16:05:55 +1000

    MVP - Record List View

--- crm/urls.py
+++ crm/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('records', views.record_list, name='record_view'),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Record
 
 def home(request):
     records = {"developer": "Jeremy Chen"}
     return render(request, 'crm/home.html', {'records': records})
+
+
+def record_list(request):
+    records = Record.objects.all()
+    return render(request, 'crm/record_list.html', {"records": records})
