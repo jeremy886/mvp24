c4987d36212ed175f7301734781142d805300ce4
jeremy886 <jeremy886@gmail.com>
2024-08-04 18:17:38 +1000

    MVP - Record Create View

--- crm/urls.py
+++ crm/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('records/', views.record_list, name='record_list'),
     path('records/<int:pk>/', views.record_detail, name='record_detail'),
+    path('records/create/', views.record_create, name='record_create'),
 ]
--- crm/views.py
+++ crm/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Record
+from .forms import RecordForm
 
 
 def home(request):
@@ -16,3 +17,13 @@ def record_detail(request, pk):
     record = get_object_or_404(Record, pk=pk)
     return render(request, 'crm/record_detail.html', {'record': record})
 
+
+def record_create(request):
+    if request.method == "POST":
+        form = RecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('record_list')  # Redirect to the list view after successful creation
+    else:
+        form = RecordForm()
+    return render(request, 'crm/record_form.html', {'form': form})
